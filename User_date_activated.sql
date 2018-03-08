e32e835423a1039bde9481205dd5b516f5c4c09c
Rene Sanchez <32004843+Rene-Sanchez@users.noreply.github.com>
2018-03-08 08:25:06 -0800

    Delete User_date_activated.sql

--- User_date_activated.sql
+++
@@ -1,5 +0,0 @@
-  SELECT use.activated_at,exp.experiment_group,COUNT(use.user_id) as user_count
-    FROM tutorial.yammer_experiments as exp
-    INNER JOIN tutorial.yammer_users as use
-    ON exp.user_id = use.user_id
-    GROUP BY use.activated_at,exp.experiment_group
